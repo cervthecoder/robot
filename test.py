8ca4c1ee870c3a58353b589fc3ac7235b5d3aac8
cervthecoder <matej.cervenka03@gmail.com>
2021-12-15 16:55:40 +0100

    Change steps to 200 in test.py

--- test.py
+++ test.py
@@ -30,25 +30,25 @@ try:
 		GPIO.output(DIR,CW)
 
 		# Run for 200 steps. This will change based on how you set you controller
-		for x in range(200):
+		for x in range(1000):
 
 			# Set one coil winding to high
 			GPIO.output(STEP,GPIO.HIGH)
 			# Allow it to get there.
-			sleep(.01) # Dictates how fast stepper motor will run
+			sleep(.004) # Dictates how fast stepper motor will run
 			# Set coil winding to low
 			GPIO.output(STEP,GPIO.LOW)
-			sleep(.01) # Dictates how fast stepper motor will run
+			sleep(.004) # Dictates how fast stepper motor will run
 
 		"""Change Direction: Changing direction requires time to switch. The
 		time is dictated by the stepper motor and controller. """
 		sleep(1.0)
 		GPIO.output(DIR,CCW)
-		for x in range(200):
+		for x in range(1000):
 			GPIO.output(STEP,GPIO.HIGH)
-			sleep(.01)
+			sleep(.004)
 			GPIO.output(STEP,GPIO.LOW)
-			sleep(0.01)
+			sleep(0.004)
 
 # Once finished clean everything up
 except KeyboardInterrupt:
